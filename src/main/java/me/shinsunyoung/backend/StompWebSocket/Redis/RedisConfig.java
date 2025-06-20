e1f17e4f55d0a82e1f0d5129d4cb3d4a220a4fbe
passionryu <rsy1225@naver.com>
2025-06-20 17:41:48 +0900

    ✨ [기능 구현] : Jenkins CICD 파이프 라인 구축
    Jenkins 파이프 라인 구축

--- src/main/java/me/shinsunyoung/backend/StompWebSocket/Redis/RedisConfig.java
+++ src/main/java/me/shinsunyoung/backend/StompWebSocket/Redis/RedisConfig.java
@@ -3,11 +3,13 @@ package me.shinsunyoung.backend.StompWebSocket.Redis;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.listener.PatternTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
+@Profile("!test")
 @Configuration
 @RequiredArgsConstructor
 public class RedisConfig {
